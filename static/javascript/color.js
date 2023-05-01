0072aa9c1bc9ace318e1a1cde3321bccf2b7f2ae
manas0726 <manasgedam26@gmail.com>
2023-05-01 22:04:44 +0530

    aws commit

--- static/javascript/color.js
+++ static/javascript/color.js
@@ -1,20 +1,18 @@
 window.onscroll = function () {
   var navbar = document.getElementById("menu-bar");
 
-
   var navMembers = navbar.getElementsByTagName("a");
 
-  if (window.pageYOffset >= 900 && window.innerWidth >= 850) {
-    navbar.style.color = "black";
-
-    for (var i = 0; i < navMembers.length; i++) {
-      navMembers[i].style.color = "black";
-    }
+  if (window.pageYOffset >= 30 && window.innerWidth >= 15) {
+    // navbar.style.color = "black";
+    navbar.style.backgroundColor = "rgba(13, 71, 161, 0.5)"; // add light black color
+    navbar.style.backdropFilter = "blur(5px)"; // add backdrop filter
   } else {
-    for (var i = 0; i < navMembers.length; i++) {
-      navMembers[i].style.removeProperty("color");
-    }
     navbar.style.color = "white";
-
+    navbar.style.removeProperty("background-color");
+    navbar.style.removeProperty("backdrop-filter"); // remove backdrop filter
   }
+  
+  navbar.style.transition = "all 0.3s ease-in-out"; // add transition
+
 };
